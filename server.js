45ca59b1b73d45683b96d1c884f79c0fe0fa829d
SlackingPalico <joeldado08@gmail.com>
2015-09-04 10:43:04 -0700

    [feat] Add Handler

--- server.js
+++ server.js
@@ -2,4 +2,13 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response){
+  response.send(200);
+})
+
+app.post('/', function(request,response){
+  response.send(201);
+})
+
 app.listen(8080);
