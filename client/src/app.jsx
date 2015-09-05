9b2926fd0e23f3f8a35835fa0846f1ac71d68e9b
Christopher Kim <christopherjhkim@gmail.com>
2015-09-04 20:45:27 -0700

    [feature] Add an instance of "InputBox"

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var InputBox = require('./inputbox');
 
-var element = React.createElement();
+var element = React.createElement(InputBox);
 React.render(element, document.querySelector('.container'));
