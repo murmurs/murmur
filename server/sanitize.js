26836c173881f6868646be109cb56a34982a9233
Uchechukwu Nnadi <ucheunnadi@gmail.com>
2015-09-08 16:56:28 -0700

    [feat] add authentication to server

--- server/sanitize.js
+++ server/sanitize.js
@@ -1,7 +1,7 @@
 //This function is used to validate that our JSON objects 
 //do not contain any HTML tags or anything that can possibly
 //be used to propagate a XSS attack
-//Our think here is that it is always good to be doubly protected
+//Our thinking here is that it is always good to be doubly protected
 //data on the client side is always a must but just in case
 //always santize the data before it entires our database
 exports.sanitizeJSON = function(jsonInput){
