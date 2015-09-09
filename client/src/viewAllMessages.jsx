511aa0ad1a8d3ed3a5730b74aedfdbb22b4e2103
Uchechukwu Nnadi <ucheunnadi@gmail.com>
2015-09-09 15:17:22 -0700

    [feat] up to date with dev branch

--- client/src/viewAllMessages.jsx
+++ client/src/viewAllMessages.jsx
@@ -23,7 +23,6 @@ var ViewAllMessages = React.createClass({
           timestamp={ message.timestamp }/>
       )
     }
-<<<<<<< HEAD
     var messageRowsSortedOptions = {
       recent: messageRows.slice().sort(function(a,b){
         return b.props.timestamp - a.props.timestamp;
@@ -36,21 +35,11 @@ var ViewAllMessages = React.createClass({
       <div style={ this.styles.messageRows }>
         { messageRowsSortedOptions[this.props.sortBy] }
       </div>
-=======
-    return (
-          <div style={this.styles.messageRows}>
-            { messageRows }
-          </div>
->>>>>>> [feat] update to voteMessage
     )
   },
   styles: {
     messageRows: {
-<<<<<<< HEAD
       padding: '10px',
-=======
-      padding: '60px',
->>>>>>> [feat] update to voteMessage
     },
   },
 });
