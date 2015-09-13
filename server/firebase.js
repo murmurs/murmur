e8b17d8fedc8b8e8ef07832346532937c51118d2
Andrew Nguyen <andrewthinhnguyen@gmail.com>
2015-09-13 02:34:42 -0700

    [feat]

--- server/firebase.js
+++ server/firebase.js
@@ -5,7 +5,7 @@ var Cookies = require('cookies');
 
 var freshPost = myDataRef.child('Fresh Post');
 
-var setTokenCookie = function (request, response, token){
+var setTokenCookie = exports.setTokenCookie = function (request, response, token){
     if(token !== undefined){
       newToken = token;
     }
@@ -14,7 +14,7 @@ var setTokenCookie = function (request, response, token){
       httpOnly: false,    // more secure but then can't access from client
     });
 
-    response.sendStatus(201)
+    response.sendStatus(200)
 }
 
 var insertPost = exports.insertPost = function(request, response, dataRef){
--- server/server.js
+++ server/server.js
@@ -11,21 +11,21 @@ app.use(bodyParser.json());
 
 app.use(Cookies.express())
 
-app.use(function(request, response, next){
-  var cookie = new Cookies(request, response);
-  if(request.cookies.get('token')){  // if there is a Token Cookie
-    console.log('request.cookies', request.cookies.get('token'))
-  } else {
-    response.cookies.set('token', tokenFactory(), { // else create Fresh Cookie token
-      maxAge: 2628000000,   // expires in 1 month
-      httpOnly: false,    // more secure but then can't access from client
-    })
-  }
-  next();
-})
+// app.use(function(request, response, next){
+//   var cookie = new Cookies(request, response);
+//   if(request.cookies.get('token')){  // if there is a Token Cookie
+//     console.log('request.cookies', request.cookies.get('token'))
+//   } else {
+//     response.cookies.set('token', tokenFactory(), { // else create Fresh Cookie token
+//       maxAge: 2628000000,   // expires in 1 month
+//       httpOnly: false,    // more secure but then can't access from client
+//     })
+//   }
+//   next();
+// })
 
 app.get('/', function(request, response){
-  response.send(200);
+  firebase.setTokenCookie(request, response)
 })
 
 app.post('/', function(request, response){ //request.body.url = 'newPost'
