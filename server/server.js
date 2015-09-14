f781c297ff70f933ee302d7622a156bca503f82c
Andrew Nguyen <andrewthinhnguyen@gmail.com>
2015-09-14 03:23:40 -0700

    [feat] Add invitation code based token distribution

--- server/server.js
+++ server/server.js
@@ -5,22 +5,39 @@ var app = express();
 var bodyParser = require('body-parser');
 var Cookies = require("cookies");
 var serverUrl = '0.0.0.0';
+var fs = require('fs')
 
 app.use('/murmur', express.static('../client'));
 app.use(bodyParser.json());
 
 app.use(Cookies.express())
 
-app.get('/noToken', function(request,response){
+app.get('/noToken', function(request, response){
+  fs.readFile('../client/src/invite.html', function(err, data){
+    if(err){
+      console.log('error reading invite.html')
+    }
+    response.setHeader('Content-Type', 'text/html')
+    response.send(data)
+  })
+})
+
+app.post('/noToken', function(request, response){
   if(request.cookies.get('token')){
     console.log('already have a token')
-  } else{                   // set Token Cookie
+    request.method = 'get';
+    // response.redirect('/murmur');
+    response.send({redirect: '/murmur'});
+  } else if(request.body.inviteCode === 'mks22'){                   // set Token Cookie
     response.cookies.set('token', tokenFactory(), {
       maxAge: 2628000000,   // expires in 1 month
       httpOnly: false,    // more secure but then can't access from client
     })
+    request.method = 'get';
+    response.send({redirect: '/murmur'});
+  } else {
+    response.send('Correct Invitation Code Required.')
   }
-  response.redirect('/murmur');
 })
 
 app.get('/', function(request, response){
