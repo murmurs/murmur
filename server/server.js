6d36f230317bdd14d16f1d275702251fde8e5d5c
Dylan Wright <dylansamuelwright@gmail.com>
2015-09-17 15:26:47 -0700

    [refactor] Updated post input to be saved on MongoDB

--- server/server.js
+++ server/server.js
@@ -24,55 +24,19 @@ var userSchema = new Schema({
   username: String,
   password: String
 });
-
-var user = mongoose.model('user', userSchema); //this is basically the users collection.
-
-var newUser = new user({username: "Dylan", password: "pw"});
-newUser.save(function(err, data) {
-  console.log("newUser data", data);
+var postSchema = new Schema({
+  userId: String,
+  username: String,
+  post: String
 });
 
-user.find(null, function(err, data){
-  console.log(">>>> " + data );
-});
 
 app.use('/murmur', express.static('../client'));
 app.use(bodyParser.json());
 
 
-
-
-// this is no longer needed
-// app.get('/noToken', function(request, response){
-//   fs.readFile('../client/src/invite.html', function(err, data){
-//     if(err){
-//       console.log('error reading invite.html')
-//     }
-//     response.setHeader('Content-Type', 'text/html')
-//     response.send(data)
-//   })
-// });
-
-//These are no longer needed because we will no longer be posting a token.
-// app.post('/noToken', function(request, response){
-//   if(request.cookies.get('token')){
-//     console.log('already have a token')
-//     request.method = 'get';
-//     // response.redirect('/murmur');
-//     response.send({redirect: '/murmur'});
-//   } else if(request.body.inviteCode === 'mks22'){  // set Token Cookie
-//     response.cookies.set('token', tokenFactory(), {
-//       maxAge: 2628000000,   // expires in 1 month
-//       httpOnly: false,    // more secure but then can't access from client
-//     })
-//     request.method = 'get';
-//     response.send({redirect: '/murmur'});
-//   } else {
-//     response.send('Correct Invitation Code Required.')
-//   }
-// })
-
 //the token needs to be set in order to access firebase.
+//this is a crappy function that should be taken out or replaced by something useful.
 app.get('/', function(request, response){
   if(request.cookies.get('token')){
     console.log('already have a token')
@@ -89,32 +53,50 @@ app.get('/', function(request, response){
   }
 });
 
+var user = mongoose.model('user', userSchema); //this is basically the users collection.
+
 app.post('/signup', function(request, response){
-  var newUser = new user({username: request.body.username, password: request.body.password});
+  var newUser = new user({
+    username: request.body.username, 
+    password: request.body.password
+  });
   newUser.save(function(err, data){
+    response.send(data);
+  });
+});
+
+var post = mongoose.model('post', postSchema);
+
+app.post('/insertPost', function(request, response) {
+  var newPost = new post({
+    userId: request.body.userId, 
+    username: request.body.username, 
+    post: request.body.post
+  });
+  newPost.save(function(err, data){
     response.send(data);
   });
 });
 
 app.post('/', function(request, response){
   firebase.insertPost(request, response);
-})
+});
 
 app.post('/comment', function(request, response){
   firebase.comment(request, response);
-})
+});
 
 app.post('/vote', function(request,response){
   firebase.votePost(request, response);
-})
+});
 
 app.post('/voteComment', function(request,response){
   firebase.voteComment(request, response);
-})
+});
 
 app.post('/favorite', function(request,response){
   firebase.toggleFavorite(request, response);
-})
+});
 
 app.listen(3000, serverUrl);
 
