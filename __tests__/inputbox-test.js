3f4e2b37f6e4b0268dcc7d27707813a7592b9643
codeCommode <james.harlan@gmail.com>
2015-09-17 20:18:53 -0700

    [chore] inputbox testing file added

--- __tests__/inputbox-test.js
+++ __tests__/inputbox-test.js
@@ -1,9 +1,8 @@
 jest.dontMock('../client/src/inputbox.jsx');
-jest.dontMock('../__tests__/parent.jsx');
+var Inputbox = require('../client/src/inputbox.jsx');
 var React = require('react/addons');
 
 var TestUtils = React.addons.TestUtils;
-var Parent = require('../__tests__/parent.jsx');
 
 beforeEach(function(){
   $ = {
@@ -17,26 +16,25 @@ describe('inputbox', function(){
   it('should create a composite component and render properly', function(){
 
   	var inputbox = TestUtils.renderIntoDocument(
-          <Parent />
+          <Inputbox />
   		);
-   //  expect(TestUtils.isCompositeComponent(inputbox)).toBeTruthy();
+    expect(TestUtils.isCompositeComponent(inputbox)).toBeTruthy();
   });
   
   it('should make ajax post on enter keystroke', function(){
 
-    // var inputbox = TestUtils.renderIntoDocument(
-    //   <InputBox />
-    //   );
-    // var input = TestUtils.findRenderedDOMComponentWithClass(inputbox, 'form-control');
-    // var form = TestUtils.findRenderedDOMComponentWithClass(inputbox, 'clearfix');
+    var inputbox = TestUtils.renderIntoDocument(
+      <Inputbox />
+      );
+    var input = TestUtils.findRenderedDOMComponentWithClass(inputbox, 'form-control');
+    var form = TestUtils.findRenderedDOMComponentWithClass(inputbox, 'clearfix');
 
-    // expect(input.getDOMNode()).toBeTruthy();
-    // expect(input.getDOMNode()).toBeTruthy();
+    expect(input.getDOMNode()).toBeTruthy();
+    expect(input.getDOMNode()).toBeTruthy();
 
-    // TestUtils.Simulate.change(input, {target : {value: 'abc123'}});
-    // TestUtils.Simulate.submit(form);
-    // console.log($.ajaxProps);
-    // expect(JSON.parse($.ajaxProps.data).message).toEqual('abc123');
+    TestUtils.Simulate.change(input, {target : {value: 'abc123'}});
+    TestUtils.Simulate.submit(form);
+    expect(JSON.parse($.ajaxProps.data).message).toEqual('abc123');
   });
 
 });
--- client/src/inputbox.jsx
+++ client/src/inputbox.jsx
@@ -1,8 +1,11 @@
-var React = require('react');
+var React = require('react/addons');
 var url = 'http://0.0.0.0:3000/';
 
 var InputBox = React.createClass({
-  
+  mixins: [React.addons.LinkedStateMixin],
+  getInitialState: function() {
+    return {message: ''};
+  },
   handleSubmit: function(event) {
     event.preventDefault(); //prevent the form from actually submitting.
     $.ajax({
@@ -13,20 +16,19 @@ var InputBox = React.createClass({
         //maybe the userId and username are set on the server side..
         "userId": "SDFSDFSDFSDF34234", //this needs to be set by the session.
         "username": "Dylan", //this needs to be set by the session.
-        "message": this.refs.newMessage.getDOMNode().value
+        "message": this.state.message,
       }),
       success: function(d){
         console.log('POST successful: ', d);
       }
     });
-    this.refs.newMessage.getDOMNode().value = '';
   },
 
   render: function() {
     return (
       <div className="input-group" style = {{padding: '15px', width: "100%"}}>
         <form onSubmit={this.handleSubmit} style={{}} className="clearfix">
-          <input type="text" ref="newMessage" className="form-control" placeholder="What's on your mind?" style={{"width":"95%", "float":"left"}}/>
+          <input type="text" valueLink={this.linkState('message')} className="form-control" placeholder="What's on your mind?" style={{"width":"95%", "float":"left"}}/>
           <span className="input-group-btn" style={{"float":"left", "width":"5%"}}>
             <input type="submit" className="btn btn-success"> Submit </input>
           </span>
