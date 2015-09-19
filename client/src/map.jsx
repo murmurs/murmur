728035a652600bd22dda0158771cfa9e96a32d72
Sin Wai Lam <sinwailam193@hotmail.com>
2015-09-19 12:07:37 -0700

    [chore] trying some code out

--- client/src/map.jsx
+++ client/src/map.jsx
@@ -1,8 +1,31 @@
 var React = require('react');
 
 var message = "<p>Hi this is a message popUp</p>"
+var arr = [{messages: 'Hi', lat: 37.77493, lng: -122.41942}, {messages: 'Hello', lat: 37.423021, lng: -122.083739}];
 
 module.exports = React.createClass({
+    addIcon: function(){
+        for(var i = 0; i < arr.length; i++){
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(arr[i].lat, arr[i].lng), 
+                map: map, 
+                icon: new google.maps.MarkerImage("http://icons.iconarchive.com/icons/custom-icon-design/mono-general-1/512/chat-icon.png", null, null, null, new google.maps.Size(40, 40))
+            });
+            var infowindow = new google.maps.InfoWindow({
+                content: '<p>' + arr[i].message + '</p>'
+            });
+            marker.addListener('click', function(){
+                map.setZoom(18);
+                map.setCenter(marker.getPosition());
+            });
+            marker.addListener('mouseover', function(){
+                infowindow.open(map, marker);
+            });
+            marker.addListener('mouseout', function(){
+                infowindow.close(map, marker);
+            });
+        }
+    },
     showMap: function(position){
         localStorage.setItem('Latitude', position.coords.latitude);
         localStorage.setItem('longitude', position.coords.longitude);
@@ -10,7 +33,7 @@ module.exports = React.createClass({
 
         var mapOptions = {
                 center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-                zoom: 12
+                zoom: 5
             },
             map = new google.maps.Map(this.getDOMNode(), mapOptions);
 
@@ -36,11 +59,27 @@ module.exports = React.createClass({
             infowindow.close(map, marker);
         });
 
-        var newMarker = new google.maps.Marker({
-            position: new google.maps.LatLng(37.77493, -122.41942), 
-            map: map, 
-            icon: new google.maps.MarkerImage("http://icons.iconarchive.com/icons/custom-icon-design/mono-general-1/512/chat-icon.png", null, null, null, new google.maps.Size(40, 40))
-        });
+        for(var i = 0; i < arr.length; i++){
+        //     arr[i].mess = new google.maps.InfoWindow({
+        //         content: '<p>' + arr[i].messages + '</p>'
+        //     });
+            new google.maps.Marker({
+                position: new google.maps.LatLng(arr[i].lat, arr[i].lng), 
+                map: map, 
+                icon: new google.maps.MarkerImage("http://icons.iconarchive.com/icons/custom-icon-design/mono-general-1/512/chat-icon.png", null, null, null, new google.maps.Size(40, 40))
+            });
+        //     arr[i].marker.addListener('mouseover', function(){
+        //         arr[i].mess.open(map, arr[i].marker);
+        //     })
+        //     arr[i].marker.addListener('mouseout', function(){
+        //         arr[i].mess.close(map, arr[i].marker);
+        //     });
+        }
+        // var newMarker = new google.maps.Marker({
+        //     position: new google.maps.LatLng(37.77493, -122.41942), 
+        //     map: map, 
+        //     icon: new google.maps.MarkerImage("http://icons.iconarchive.com/icons/custom-icon-design/mono-general-1/512/chat-icon.png", null, null, null, new google.maps.Size(40, 40))
+        // });
     },
     getLocation: function(){
         if(navigator.geolocation){
